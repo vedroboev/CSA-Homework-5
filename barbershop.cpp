f76bf8967a3f8386b5ffe643a75d966c097329af
vedroboev <ftabaev_1@edu.hse.ru>
2021-12-16 16:57:02 +0300

    Reformat code, add comments

--- barbershop.cpp
+++ barbershop.cpp
@@ -1,29 +1,62 @@
+// Definitions for the various barbershop methods and constants.
 #include "barbershop.h"
 
-const int max_customer_count = 10;
-const int haircut_time_bound = 10;
+//----------------------------------------------------------------------------------------------------------------------
+// VARIABLES.
+//----------------------------------------------------------------------------------------------------------------------
 
+// Max customer count and the upper limit to a haircut duration, day duration.
+const int max_customer_count = 50;
+// These are measured in seconds.
+int haircut_time_bound = 10;
+int day_duration = 120;
+
+// Variables for keeping track of customers in the waiting room.
 int total_customer_count = 0;
 int waiting_room_customer_count = 0;
 int new_customer_index = 0;
 
+// Semaphore for the customers waiting in the waiting room.
 sem_t customers_waiting;
 
+// Mutex for modifying waiting room data.
 pthread_mutex_t incrementing;
+// Mutex for when the barber is cutting his hair.
 pthread_mutex_t cutting;
+// Mutex for when the barber is busy.
 pthread_mutex_t busy;
+// Mutex for when the customer is leaving the shop.
 pthread_mutex_t leaving;
 
+//----------------------------------------------------------------------------------------------------------------------
+// UTILITY METHODS.
+//----------------------------------------------------------------------------------------------------------------------
+
+// Initializes semaphore and mutex variables.
+void initializeSemaphores(){
+    sem_init(&customers_waiting, 0, 0);
+    pthread_mutex_init(&incrementing, nullptr);
+    pthread_mutex_init(&cutting, nullptr);
+    pthread_mutex_init(&busy, nullptr);
+    pthread_mutex_init(&leaving, nullptr);
+    // Leaving and busy need to be locked from the beginning.
+    pthread_mutex_lock(&leaving);
+    pthread_mutex_lock(&busy);
+}
+
+// Utility for working with cutting the customer's hair.
 void cutHair() {
     printf("[BARBER] Haircut for customer %d started...\n", total_customer_count);
     // Picking a random haircut time.
     int time = rand() % haircut_time_bound + 1; // NOLINT
     // Waiting unlit haircut end.
     sleep(time);
+    // Signalling the end of the haircut.
     printf("[BARBER] Haircut for customer %d finished! \n\tTotal haircut time: %d.\n", total_customer_count, time);
     pthread_mutex_unlock(&cutting);
 }
 
+// Utility for getting the hair cut + leaving the store.
 void getHaircut(int customer_index) {
     // Waiting until the haircut is finished.
     pthread_mutex_lock(&cutting);
@@ -34,8 +67,19 @@ void getHaircut(int customer_index) {
     pthread_mutex_unlock(&cutting);
 }
 
+//----------------------------------------------------------------------------------------------------------------------
+// MAIN HANDLER METHODS.
+//----------------------------------------------------------------------------------------------------------------------
+
+// Barber thread function.
 void *barber(void *parameter) {
-    while (total_customer_count < 10) {
+    // Keeping track of time.
+    time_t start, end;
+    time(&start);
+    time(&end);
+
+    // Main body loop, doing this until the working day ends (we'll serve the latest customer even after closing).
+    while (difftime(end, start) < day_duration) {
         // Sleeping until a customer appears.
         sem_wait(&customers_waiting);
 
@@ -44,7 +88,6 @@ void *barber(void *parameter) {
         // Incrementing total visitor count, decrementing the number of people in the waiting room.
         waiting_room_customer_count--;
         total_customer_count++;
-        printf("---------------------------------------\n");
         printf("[BARBER] Calling customer %d from the waiting room...\n\tTotal people in waiting room: %d\n",
                total_customer_count,
                waiting_room_customer_count);
@@ -52,11 +95,10 @@ void *barber(void *parameter) {
         pthread_mutex_unlock(&incrementing);
 
         // Make the barber available.
-
         pthread_mutex_unlock(&busy);
+
         // Lock the cutting mutex and cut the customer's hair.
         pthread_mutex_lock(&cutting);
-
         cutHair();
 
         // Waiting until the customer is done.
@@ -65,16 +107,17 @@ void *barber(void *parameter) {
     return nullptr;
 }
 
+// Customer thread function.
 void *customer(void *parameter) {
     // Waiting until waiting room can be modified.
     pthread_mutex_lock(&incrementing);
-    // Adding the current customer to the line.
+    // Adding the current customer to the line, getting the index of the current customer.
     waiting_room_customer_count++;
     int current_index = ++new_customer_index;
-    printf("[CUSTOMER] Customer %d entered the waiting room...\n\tTotal people in waiting room: %d\n",
+    printf("[WAITING ROOM] Customer %d entered the waiting room...\n\tTotal people in waiting room: %d\n",
            new_customer_index,
            waiting_room_customer_count);
-    // Unlocking modification and incrementing waiting line.
+    // Unlocking modification.
     pthread_mutex_unlock(&incrementing);
 
     // Posting to the line (which indicates that there's a customer available).
@@ -83,6 +126,7 @@ void *customer(void *parameter) {
     // Waiting until the barber is available.
     pthread_mutex_lock(&busy);
 
+    // Getting the haircut (and leaving).
     getHaircut(current_index);
 
     return nullptr;
--- barbershop.h
+++ barbershop.h
@@ -1,17 +1,24 @@
 #ifndef CSA_HOMEWORK_5__BARBERSHOP_H_
 #define CSA_HOMEWORK_5__BARBERSHOP_H_
 
+// HEADERS.
+
 #include "stdio.h" // NOLINT
 #include "stdlib.h" // NOLINT
 #include "unistd.h"
 #include "semaphore.h"
 #include "pthread.h"
 
-extern const int max_customer_count;
-extern const int haircut_time_bound;
+// VARIABLES.
 
+extern const int max_customer_count;
+
+extern int haircut_time_bound;
+extern int day_duration;
 extern int total_customer_count;
 extern int waiting_room_customer_count;
+
+// SEMAPHORE VARIABLES.
 
 extern sem_t customers_waiting;
 
@@ -20,7 +27,14 @@ extern pthread_mutex_t cutting;
 extern pthread_mutex_t busy;
 extern pthread_mutex_t leaving;
 
+void initializeSemaphores();
+
+// UTILITY METHODS.
+
 void cutHair();
+void getHaircut(int customer_index);
+
+// MAIN HANDLER METHODS.
 
 void *barber(void *parameter);
 void *customer(void *parameter);
